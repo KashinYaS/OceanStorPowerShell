eb6fff3cb7b030e52a67a8e47b48d0b8ac047ef7
Yakov Kashin <kashinyas@gmail.com>
2021-11-18 11:45:12 +0500

    Update Get-OceanStorLUNGroup.ps1
    Added LUN Group search by member LUN's ID.
    Fixed bug with ID = 0 working as ID = $null (but only in Get-OceanStorLUNGroup)

--- OceanStorPowerShellPS/Get-OceanStorLUNGroup.ps1
+++ OceanStorPowerShellPS/Get-OceanStorLUNGroup.ps1
@@ -1,19 +1,19 @@
 Function Get-OceanStorLUNGroup {
   [CmdletBinding(DefaultParameterSetName="Default")]
   PARAM (
-    [PARAMETER(Mandatory=$True, Position=0,HelpMessage = "OceanStor's FQDN or IP address",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][String]$OceanStor,
-    [PARAMETER(Mandatory=$False,Position=1,HelpMessage = "Port",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][int]$Port=8088,	
-    [PARAMETER(Mandatory=$True, Position=2,HelpMessage = "Username",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][String]$Username,
-    [PARAMETER(Mandatory=$True, Position=3,HelpMessage = "Password",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][String]$Password,
-    [PARAMETER(Mandatory=$False,Position=4,HelpMessage = "Scope (0 - internal users, 1 - LDAP users)",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][int]$Scope=0,
-    [PARAMETER(Mandatory=$False,Position=5,HelpMessage = "Silent - if set then function will not show error messages",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][bool]$Silent=$true,
-    [PARAMETER(Mandatory=$True,Position=6,HelpMessage = "LUN Group name",ParameterSetName='LUNGroupName')][String]$Name = $null,
-    [PARAMETER(Mandatory=$True,Position=6,HelpMessage = "LUN Group ID",ParameterSetName='ID')][int]$ID = $null
+    [PARAMETER(Mandatory=$True, Position=0,HelpMessage = "OceanStor's FQDN or IP address",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][String]$OceanStor,
+    [PARAMETER(Mandatory=$False,Position=1,HelpMessage = "Port",                          ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][int]$Port=8088,	
+    [PARAMETER(Mandatory=$True, Position=2,HelpMessage = "Username",                      ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][String]$Username,
+    [PARAMETER(Mandatory=$True, Position=3,HelpMessage = "Password",                      ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][String]$Password,
+    [PARAMETER(Mandatory=$False,Position=4,HelpMessage = "Scope (0 - internal users, 1 - LDAP users)",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][int]$Scope=0,
+    [PARAMETER(Mandatory=$False,Position=5,HelpMessage = "Silent - if set then function will not show error messages",ParameterSetName='Default')][PARAMETER(ParameterSetName='ID')][PARAMETER(ParameterSetName='LUNGroupName')][PARAMETER(ParameterSetName='LUNID')][bool]$Silent=$true,
+    [PARAMETER(Mandatory=$True, Position=6,HelpMessage = "LUN Group name",                ParameterSetName='LUNGroupName')][String]$Name = $null,
+    [PARAMETER(Mandatory=$True, Position=6,HelpMessage = "LUN Group ID",                  ParameterSetName='ID')][int]$ID = $null,
+	[PARAMETER(Mandatory=$True, Position=6,HelpMessage = "LUN ID",                        ParameterSetName='LUNID')][int]$LUNID = $null
   )
   $RetVal = $null
  
   # --- prepare to connect with TLS 1.2 and ignore self-signed certificate of OceanStor ---
-  [Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
 
   Fix-OceanStorConnection
 
@@ -33,14 +33,19 @@ Function Get-OceanStorLUNGroup {
 
     $UserCredentials = New-Object System.Management.Automation.PsCredential("$($Username)",$(ConvertTo-SecureString -String "$($Password)" -AsPlainText -force))
 
-    # --- getting existing hosts 
-    if (-not $ID) {
-	  $URI = $RESTURI  + "lungroup"
-	}
-	else {
-	  $URI = $RESTURI  + "lungroup/" + $ID
-	}
-	
+
+    switch ( $PSCmdlet.ParameterSetName )
+    {
+      'ID' { $URI = $RESTURI  + "lungroup/" + $ID }
+	  'LUNID' {
+	    $URI = $RESTURI  + "lungroup/associate?ASSOCIATEOBJTYPE=11&ASSOCIATEOBJID=" + $LUNID
+	  }
+	  default { 
+	    # no LUN or Group ID specified, retrieving all groups or searching group by name
+	    $URI = $RESTURI  + "lungroup" 
+	  }
+    }
+		
     $result = Invoke-RestMethod -Method "Get" $URI -Headers $header -ContentType "application/json" -Credential $UserCredentials -WebSession $WebSession
     if ($result -and ($result.error.code -eq 0)) {
       if (-not $Name) {
@@ -66,4 +71,5 @@ Function Get-OceanStorLUNGroup {
   
   Return($RetVal)
 }
+
 
